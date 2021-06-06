693995aab224756db1adfedf7ed54e8b87848faa
TLDK-1 <thierry.dlira@gmail.com>
2021-06-05 23:33:29 -0300

    1
    Begin

---
+++ Assets/Aligned Games/First Person Character System/System/Scripts/Managers/ColliderManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderManager : MonoBehaviour
+
+{
+
+    public float Damage;
+
+    public void OnTriggerStay(Collider other)
+
+    {
+
+        if (other.gameObject.tag == "Player")
+
+        {
+
+            other.GetComponent<PlayerHealthManager>().CurrentHealth -= Damage;
+
+        }
+
+    }
+
+}
